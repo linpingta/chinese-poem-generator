fcf3420aa29a642d3623df971c77e79851b78d15
linpingta <chut1987@gmail.com>
2016-12-15 12:19:17 +0800

    upload to pypi

--- poem_generator/setup.py
+++ poem_generator/setup.py
@@ -1,14 +1,15 @@
 from setuptools import setup
 
-setup(name='chinese_poem_generator',
+setup(name='ts_chinese_poem_generator',
 	version='0.2',
 	description='Chinese Poem Generator',
 	url="https://github.com/linpingta/chinese-poem-generator.git",
 	author='ChuTong',
 	author_email='linpingta@163.com',
 	license='MIT',
-	packages=['poem_generator'],
+	packages=['ts_chinese_poem_generator'],
 	install_requires=[
 		"jieba", "gensim"
 	],
+	keywords=["chinese", "poem"],
 	zip_safe=False)
